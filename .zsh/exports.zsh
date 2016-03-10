0c3c53f128eddad559e7ac1770ab79f03753d282
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-03-10 13:21:18 +0300

    Configure zsh

---
+++ .zsh/exports.zsh
@@ -0,0 +1,10 @@
+# Currently this path is appendend to dynamically when picking a ruby version
+export PATH=node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:~/.cabal/bin:~/.local/bin:$PATH
+export PATH=$PATH:/opt/boxen/homebrew/opt/go/libexec/bin
+
+# Setup terminal, and turn on colors
+export TERM=xterm-256color
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+export EDITOR='vim'
---
+++ zshrc
@@ -0,0 +1,5 @@
+source ~/.dotfiles/.zsh/completion.zsh
+source ~/.dotfiles/.zsh/exports.zsh
+source ~/.dotfiles/.zsh/history.zsh
+source ~/.dotfiles/.zsh/prompt.zsh
+source ~/.dotfiles/.zsh/setopt.zsh
