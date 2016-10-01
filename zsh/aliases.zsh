0160cf21d84a01f0585d026bdb5bc9eba2ea56e8
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2016-10-01 14:04:39 +0300

    Remove os-specific and rare usable aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -32,39 +32,12 @@ alias gup="git pull --rebase --prune $@ && git submodule update --init --recursi
 alias gbclean = "f() { git branch --merged ${1-master} | grep -v " ${1-master}$" | xargs git branch -d; }; f"
 alias gbdone = "f() { git checkout ${1-master} && git up && git bclean ${1-master}; }; f"
 
-# Bundler Commands
-alias be='bundle exec'
+# Bundler and Rails Commands
 alias bi='bundle install'
-alias bl='bundle list'
-alias bu='bundle update'
-alias bp='bundle package'
-
-# Rails Commands
-alias r='rails'
-alias rs='rails s'
-alias rc='rails c'
-alias dbm='rake db:migrate'
-
-# Brew services
-alias bs="brew services"
-
-# Editors
-alias sublime="open -a /Applications/Sublime\ Text.app"
-alias subl="open -a /Applications/Sublime\ Text.app"
+alias b="bundle exec"
+alias c="bundle exec cap"
+alias r="bundle exec rails"
 
 # Other
-alias smi="sudo make install"
 alias please="sudo"
-alias pig="ping google.com"  # Testing internet connection
-
-# Desktop Programs
-alias f='open -a Finder '
-alias photoshop="open -a '/Applications/Adobe\ Photoshop\ CC\ 2015/Adobe\ Photoshop\ CC\ 2015.app'"
-alias sketch="open -a '/Applications/Sketch.app"
-alias preview="open -a '$PREVIEW'"
-alias xcode="open -a '/Applications/XCode.app'"
-alias safari="open -a safari"
-alias chrome="open -a google\ chrome"
-alias skype='open -a Skype'
-alias slack='open -a Slack'
-alias telegram='open -a Telegram'
+alias pig="ping google.com -t 5"
