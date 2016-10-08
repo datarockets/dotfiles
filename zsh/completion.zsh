4800fbd719f4fe80eeedd6b19a8503e2e4b596b6
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2016-10-08 15:42:18 +0300

    Simplify completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,25 +1,4 @@
-# add in zsh-completions
-fpath=(/opt/boxen/homebrew/share/zsh-completions $fpath)
+fpath=(/usr/local/share/zsh/site-functions $fpath)
 
-autoload -U compinit && compinit                                                                                       
-zmodload -i zsh/complist
-
-# Enable completion caching, use rehash to clear
-zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
-
-# Fallback to built in ls colors
-zstyle ':completion:*' list-colors ''
-
-# Make the list prompt friendly
-zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
-
-# Make the selection prompt friendly when there are a lot of choices
-zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-
-# Add simple colors to kill
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-
-# list of completers to use
-zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
-zstyle ':completion:*' menu select=1 _complete _ignored _approximate
+autoload -U compinit
+compinit
