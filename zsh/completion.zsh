c3ed45185b464916c2af948328f1a947d33f9471
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2016-10-08 17:20:07 +0300

    Improve completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,6 +3,14 @@ fpath=(/usr/local/share/zsh/site-functions $fpath)
 autoload -U compinit
 compinit
 
+setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
-setopt auto_menu
+
+zstyle ':completion:*:*:*:*:*' menu select # menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*' # case insensitive search
+zstyle ':completion:*' list-colors '' # colored completions
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache
