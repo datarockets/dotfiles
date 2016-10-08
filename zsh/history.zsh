193f965dab334b0b98e28d838ca91ac3a7eec07e
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2016-10-08 15:59:51 +0300

    Use oh-my-zsh history settings

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,6 +1,14 @@
-# HISTORY
+HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
-HISTFILE=~/.zsh_history
 
-bindkey '^R' zaw-history
+# See "History" section in https://linux.die.net/man/1/zshoptions
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt inc_append_history
+setopt share_history
+
+alias history="fc -il 1" # show human readable timestamps
--- zshrc
+++ zshrc
@@ -1,10 +1,10 @@
 source ~/.zsh/colors.zsh
 source ~/.zsh/completion.zsh
-
-
-
-
 source ~/.zsh/history.zsh
+
+
+
+
 source ~/.zsh/prompt.zsh
 source ~/.zsh/setopt.zsh
 
